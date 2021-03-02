905242b5f49ad34b34f9a052e06ee239115bd6fe
mde11 <mde11@10.38.22.20>
2021-03-01 20:17:20 -0500

    Added fitglm() function for several continuous distributions and tested for minimum AICc to find best fit distribution and display results from that model.

--- stats_genotype_gender.m
+++ stats_genotype_gender.m
@@ -1,2 +1,11 @@
 %stats_genotype_gender script
 %test script for running stats for when gender = true in data set
+
+%stats by gender and genotype (if gender = true in function input)
+for ii = 1:size(NMJ_array,2)
+%test generalized linear model with several distributions for each set of
+%measurements - poisson, normal, lognormal, gamma
+%also need to test beta distribution fit - separate command
+fitglm(NMJ_array(:,ii));
+%how to add model: Measurement~Genotype*Gender
+end
