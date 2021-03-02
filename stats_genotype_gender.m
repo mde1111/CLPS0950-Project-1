7ecb5a2f3ee95c9133a04ae64c6f3259183d394a
mde11 <mde11@10.38.22.20>
2021-03-02 14:55:00 -0500

    Updated function to run stats analysis on NMJ data with input argument for whether analysis by genotype and gender or just genotype

--- stats_genotype_gender.m
+++ stats_genotype_gender.m
@@ -1,11 +1,130 @@
-%stats_genotype_gender script
-%test script for running stats for when gender = true in data set
+function [data_signif] = stats_genotype_gender(NMJ_data, gender)
+%NMJ_GENOTYPE_STATS Find best fit distribution and runs stats on NMJ
+%morphology data, prints output and signficance of generalized linear model
+%with best fit distribution by genotype and gender
+%Input: NMJ_data = data table to analyze; gender = true or false
+%Output: data_signif = boolean vector of signficance by genotype
+% Created by K2M for Project 1 on 03.02.21
 
-%stats by gender and genotype (if gender = true in function input)
-for ii = 1:size(NMJ_array,2)
-%test generalized linear model with several distributions for each set of
-%measurements - poisson, normal, lognormal, gamma
-%also need to test beta distribution fit - separate command
-fitglm(NMJ_array(:,ii));
-%how to add model: Measurement~Genotype*Gender
+%eventually make output a table of results?
+
+%sets columns from data table want to analyze
+col_nums = [2:8, 14:18];
+data_signif = [];
+
+if ~gender
+    %for loop to run through analysis for each column in col_nums
+    for jj = col_nums
+        col_name = char(NMJ_data.Properties.VariableNames(jj));
+        geno_var = {' ~ Genotype'};
+        mod_cell = strcat(col_name, geno_var);
+        mod_spec = char(mod_cell);
+        NMJ_log = NMJ_data;
+        NMJ_log(1:end, col_name) = varfun(@log,NMJ_log,'InputVariables',col_name);
+
+        %specify model (eventually use for loop to go through all columns?)
+        %fit generalized linear model with possible distributions - normal, gamma,
+        %lognormal, poisson
+        modelspec = (mod_spec);
+        AIC_vec = [];
+        norm_mod = fitglm(NMJ_data, modelspec, 'Distribution', 'normal');
+        AIC_vec = [AIC_vec, norm_mod.ModelCriterion.AICc];
+        gam_mod = fitglm(NMJ_data, modelspec, 'Distribution', 'gamma');
+        AIC_vec = [AIC_vec, gam_mod.ModelCriterion.AICc];
+        log_mod = fitglm(NMJ_log, modelspec, 'Distribution', 'normal');
+        AIC_vec = [AIC_vec, log_mod.ModelCriterion.AICc];
+        pois_mod = fitglm(NMJ_data, modelspec, 'Distribution', 'poisson');
+        AIC_vec = [AIC_vec, pois_mod.ModelCriterion.AICc];
+
+        %find proper distribution of data based on minimum AICc and save as
+        %variable data_dist
+        %print results from fitglm model of selected best-fit distribution
+        [row, col] = find(AIC_vec == min(AIC_vec)); %#ok<*ASGLU>
+        if col == 1
+            data_dist = ('Normal');
+            data_mod = norm_mod;
+        elseif col == 2
+            data_dist = ('Gamma');
+            data_mod = gam_mod;
+        elseif col == 3
+            data_dist = ('Lognormal');
+            data_mod = log_mod;
+        elseif col == 4
+            data_dist = ('Poisson');
+            data_mod = pois_mod;
+        else
+            data_dist = ('Unknown');
+        end
+        disp(data_dist);
+        disp(data_mod);
+        p_val = data_mod.Coefficients.pValue(2);
+        disp(p_val);
+    
+        %assigns boolean variable to data_signif for each column analyzed,
+        %where true = statistically signficant difference by genotype
+        if p_val <= 0.05
+            signif = true;
+        else
+            signif = false;
+        end
+    
+        data_signif = [data_signif, signif];
+    end
+else
+    %for loop to run through analysis for each column in col_nums
+    for jj = col_nums
+        col_name = char(NMJ_data.Properties.VariableNames(jj));
+        geno_var = {' ~ Genotype * Gender'};
+        mod_cell = strcat(col_name, geno_var);
+        mod_spec = char(mod_cell);
+        NMJ_log = NMJ_data;
+        NMJ_log(1:end, col_name) = varfun(@log,NMJ_log,'InputVariables',col_name);
+
+        %specify model (eventually use for loop to go through all columns?)
+        %fit generalized linear model with possible distributions - normal, gamma,
+        %lognormal, poisson
+        modelspec = (mod_spec);
+        AIC_vec = [];
+        norm_mod = fitglm(NMJ_data, modelspec, 'Distribution', 'normal');
+        AIC_vec = [AIC_vec, norm_mod.ModelCriterion.AICc];
+        gam_mod = fitglm(NMJ_data, modelspec, 'Distribution', 'gamma');
+        AIC_vec = [AIC_vec, gam_mod.ModelCriterion.AICc];
+        log_mod = fitglm(NMJ_log, modelspec, 'Distribution', 'normal');
+        AIC_vec = [AIC_vec, log_mod.ModelCriterion.AICc];
+        pois_mod = fitglm(NMJ_data, modelspec, 'Distribution', 'poisson');
+        AIC_vec = [AIC_vec, pois_mod.ModelCriterion.AICc];
+
+        %find proper distribution of data based on minimum AICc and save as
+        %variable data_dist
+        %print results from fitglm model of selected best-fit distribution
+        [row, col] = find(AIC_vec == min(AIC_vec)); %#ok<*ASGLU>
+        if col == 1
+            data_dist = ('Normal');
+            data_mod = norm_mod;
+        elseif col == 2
+            data_dist = ('Gamma');
+            data_mod = gam_mod;
+        elseif col == 3
+            data_dist = ('Lognormal');
+            data_mod = log_mod;
+        elseif col == 4
+            data_dist = ('Poisson');
+            data_mod = pois_mod;
+        else
+            data_dist = ('Unknown');
+        end
+        disp(data_dist);
+        disp(data_mod);
+        p_val = data_mod.Coefficients.pValue(2);
+        disp(p_val);
+    
+        %assigns boolean variable to data_signif for each column analyzed,
+        %where true = statistically signficant difference by genotype
+        if p_val <= 0.05
+            signif = true;
+        else
+            signif = false;
+        end
+        data_signif = [data_signif, signif];
+    end
 end
