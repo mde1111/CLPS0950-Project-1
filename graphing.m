c8d3a01244e0c6516c592020276b666f70de120b
mariarodriguez <mariarodriguez@marias-mbp.devices.brown.edu>
2021-03-04 13:19:38 -0500

    I am thinking of making it interactive, what do you all think?

--- graphing.m
+++ graphing.m
@@ -1,7 +1,29 @@
+% ask which x and which variable they would like to see
+disp('For the following question, input one of the following options:')
+xvar = input('What would you like the x variable to be?')
+ 
+disp('For the following question, input one of the following options:')
+yvar = input('What you you like the y varibale to be')
+
+disp('For the following question, input one of the following options: bar, scatter')
+graphtype = input('What type of graph would you like?')
+
+another = input('Would you like to request an additional graph?')
+%if another = 'yes'
+    %ask questions again
+    %else proceed to display graph 
+    
+
+    
 %Graph of results - dotplots and/or density curves of most measurements
+scatter(x,y)
+title()
+xlabel()
+ylabel()
+
 
 %Graph across lifespan - graph to look at how measurements change with age
-%graph for each measuremnt (column 4
+
 graphs = figure;
 xlsread(monthSTMdata)
 x = importedxdata;
@@ -25,3 +47,4 @@ subplots(1,3,3)
 xlabel('name of variable')
 ylabel('name of variable')
 title('x v y')
+
