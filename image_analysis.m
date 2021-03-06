dafe78301af9905cb90427af70112fe5a29c2f4a
mde11 <mde11@10.38.22.20>
2021-03-05 20:17:57 -0500

    Updated image_analysis script to skeletonize presynaptic channel and calculate branching and complexity of morphology. Written by ME, time = 2.0 hours.

--- image_analysis.m
+++ image_analysis.m
@@ -1,16 +1,23 @@
+%written by Madison Ewing on 03.04.21
+%morphological measurements used to analyzed NMJs adapted from NMJ morph
+%macro workflow (Jones et al. 2016), which can be found at this link:
+%https://royalsocietypublishing.org/doi/10.1098/rsob.160240#RSOB160240C32
+
 pixels_per_um = 10.91;
 %um_length = pixel_length / pixels_per_um; % Convert length in pixels to um
 
+%load image, which has presynaptic (1) and postsynaptic (2) channels
 axon_terminal = imread('MAX_021421_animalFB_40x_01-1.tif',2);
 muscle_endplate = imread('MAX_021421_animalFB_40x_01-1.tif',1);
-imshow(axon_terminal);
+
+%threshold, filter, and make binary nerve terminal
 axon_thresh = graythresh(axon_terminal);
 axon_med = medfilt2(axon_terminal);
-axon_thresh = graythresh(axon_terminal);
 bw_axon = imbinarize(axon_med, axon_thresh);
 axon_filt = medfilt2(bw_axon);
 axon_filt_2 = wiener2(axon_filt,[5 5]);
-imshow(axon_filt_2);
+
+%find area and perimeter of axon terminal
 axon_area = bwarea(axon_filt_2);
 axon_area_um2 = axon_area / (pixels_per_um^2);
 axon_perim_img = bwperim(axon_filt_2);
@@ -19,14 +26,41 @@ axon_perim_um = axon_perim / pixels_per_um;
 disp(axon_area_um2);
 disp(axon_perim_um);
 
-imshow(muscle_endplate);
+%skeletonize axon terminal and perform branching analysis
+axon_skel = bwskel(axon_filt_2);
+imshow(axon_skel);
+count_0 = 0;
+count_2 = 0;
+
+for ii = 2:size(axon_skel,1)
+    for jj = 2:size(axon_skel,2)
+        pixel_connect = sum(axon_skel(jj-1:jj+1,ii-1:ii+1))-axon_skel(ii,jj);
+        if pixel_connect == 0
+            count_0 = count_0 + 1;
+        elseif pixel_connect == 2
+            count_2 = count_2 + 1;
+        elseif pixel_connect == 4;
+            count_4 = count_4 + 1;
+        elseif pixel_connect = 5;
+            count_5 = count_5 + 1;
+        end
+    end
+end
+term_branch_num = count_2;
+branch_point_num = (count_4 + count_5) * 0.28;
+total_branch_leng = (size(axon_skel,1)^2 - count_0) / pixels_per_um;
+avg_branch_leng = total_branch_leng / count_2;
+complexity = log10(count_2 * branch_point_num * total_branch_length);
+
+%threshold, filter, and make binary muscle endplate
 endplate_thresh = graythresh(muscle_endplate);
 endplate_med = medfilt2(muscle_endplate);
 endplate_thresh = graythresh(muscle_endplate);
 bw_endplate = imbinarize(endplate_med, endplate_thresh);
 endplate_filt = medfilt2(bw_endplate);
 endplate_filt_2 = wiener2(endplate_filt,[5 5]);
-imshow(endplate_filt_2);
+
+%find area and perimeter of endplate
 endplate_area = bwarea(endplate_filt_2);
 endplate_perim = bwperim(endplate_filt_2);
 endplate_area_um2 = endplate_area / (pixels_per_um^2);
